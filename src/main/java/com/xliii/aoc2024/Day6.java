8cc9fafdb52c855155850bd9bed4bbe11dbe8551
xliii <xliii@mail.com>
2024-12-05 14:01:50 +0200

    Day 6 stub

---
+++ src/main/java/com/xliii/aoc2024/Day6.java
@@ -0,0 +1,23 @@
+package com.xliii.aoc2024;
+
+public class Day6 extends Puzzle {
+
+    @Override
+    protected boolean useExample() {
+        return true;
+    }
+
+    @Override
+    protected void run() {
+        solve1();
+    }
+
+    private void solve1() {
+
+    }
+
+    public static void main(String[] args) {
+        Day6 puzzle = new Day6();
+        puzzle.run();
+    }
+}
