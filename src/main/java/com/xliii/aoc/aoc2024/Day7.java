1cea0fbe8a95c014bcb74ad7762c9ba706e7cbc7
xliii <xliii@mail.com>
2024-12-06 18:10:13 +0200

    Day 7 prep

---
+++ src/main/java/com/xliii/aoc/aoc2024/Day7.java
@@ -0,0 +1,30 @@
+package com.xliii.aoc.aoc2024;
+
+import com.xliii.aoc.Puzzle;
+
+public class Day7 extends Puzzle {
+
+    @Override
+    protected boolean useExample() {
+        return true;
+    }
+
+    @Override
+    protected void run() {
+        solve1();
+        solve2();
+    }
+
+    private void solve1() {
+
+    }
+
+    private void solve2() {
+
+    }
+
+    public static void main(String[] args) {
+        Day7 puzzle = new Day7();
+        puzzle.run();
+    }
+}
