f31087defd72d82e470404d379bcead959f4830e
xliii <xliii@mail.com>
2024-12-12 11:55:26 +0200

    Day 13 stub

---
+++ src/main/java/com/xliii/aoc/aoc2024/Day13.java
@@ -0,0 +1,30 @@
+package com.xliii.aoc.aoc2024;
+
+import com.xliii.aoc.Puzzle;
+
+public class Day13 extends Puzzle {
+
+    @Override
+    protected boolean useExample() {
+        return true;
+    }
+
+    @Override
+    protected void run() {
+        solve1();
+        solve2();
+    }
+
+    private void solve2() {
+
+    }
+
+    private void solve1() {
+
+    }
+
+    public static void main(String[] ignoredArgs) {
+        Day13 puzzle = new Day13();
+        puzzle.run();
+    }
+}
