3bcd899b777e1e5eb8d9fbdbfcdaae74d53ac149
xliii <xliii@mail.com>
2024-12-16 12:33:55 +0200

    Color refactoring, custom & grayscale colors

--- src/main/java/com/xliii/aoc/util/Color.java
+++ src/main/java/com/xliii/aoc/util/Color.java
@@ -1,24 +1,45 @@
 package com.xliii.aoc.util;
 
-public enum Color {
-    RED("\u001B[31m"),
-    GREEN("\u001B[32m"),
-    YELLOW("\u001B[33m"),
-    BLACK("\u001b[30m"),
-    BLUE("\u001b[34m"),
-    MAGENTA("\u001b[35m"),
-    CYAN("\u001b[36m"),
-    WHITE("\u001b[37m");
+public class Color {
+    public static final Color BLACK = new Color("0");
+    public static final Color RED = new Color("1");
+    public static final Color GREEN = new Color("2");
+    public static final Color YELLOW = new Color("3");
+    public static final Color BLUE = new Color("4");
+    public static final Color MAGENTA = new Color("5");
+    public static final Color CYAN = new Color("6");
+    public static final Color WHITE = new Color("7");
 
+    private static final String PREFIX = "\u001B[";
+    private static final String PREFIX_FOREGROUND = PREFIX + "3";
+    private static final String POSTFIX = "m";
+    private static final String RESET = PREFIX + "0" + POSTFIX;
+    private static final String CUSTOM_SUFFIX = "8;5;";
+    private static final int GRAYSCALE_OFFSET = 232;
 
-    private static final String RESET = "\u001B[0m";
     private final String ansiColor;
 
-    Color(String ansiColor) {
-        this.ansiColor = ansiColor;
+    private Color(String ansiColor) {
+        this.ansiColor = PREFIX_FOREGROUND + ansiColor + POSTFIX;
     }
 
     public String text(Object text) {
         return ansiColor + text + RESET;
+    }
+
+    public static Color grayscale(int color) {
+        if (color < 0 || color > 32) {
+            throw new IllegalArgumentException("Invalid grayscale color: " + color + ". Available values: 0-23");
+        }
+
+        return new Color(CUSTOM_SUFFIX + (color + GRAYSCALE_OFFSET));
+    }
+
+    public static Color custom(int color) {
+        if (color < 0 || color > 255) {
+            throw new IllegalArgumentException("Invalid custom color: " + color + ". Available values: 0-255");
+        }
+
+        return new Color(CUSTOM_SUFFIX + color);
     }
 }
