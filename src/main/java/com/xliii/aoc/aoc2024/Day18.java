4eadec0423215b75a017f1376a185d0e96873fad
xliii <xliii@mail.com>
2024-12-17 16:02:45 +0200

    Day 18 stub

---
+++ src/main/java/com/xliii/aoc/aoc2024/Day18.java
@@ -0,0 +1,30 @@
+package com.xliii.aoc.aoc2024;
+
+import com.xliii.aoc.Puzzle;
+
+public class Day18 extends Puzzle {
+
+    @Override
+    protected boolean useExample() {
+        return true;
+    }
+
+    @Override
+    protected void run() {
+        solve1();
+        solve2();
+    }
+
+    private void solve2() {
+
+    }
+
+    private void solve1() {
+
+    }
+
+    public static void main(String[] ignoredArgs) {
+        Day18 puzzle = new Day18();
+        puzzle.run();
+    }
+}
