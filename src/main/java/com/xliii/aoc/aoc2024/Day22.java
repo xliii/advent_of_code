5e114cd251a03badd3b46c01bf636b0f0ff2ce45
xliii <xliii@mail.com>
2024-12-22 10:29:59 +0200

    Day 22.1

--- src/main/java/com/xliii/aoc/aoc2024/Day22.java
+++ src/main/java/com/xliii/aoc/aoc2024/Day22.java
@@ -4,9 +4,11 @@ import com.xliii.aoc.Puzzle;
 
 public class Day22 extends Puzzle {
 
+    private static final long ITERATIONS = 2000;
+
     @Override
     protected boolean useExample() {
-        return true;
+        return false;
     }
 
     @Override
@@ -15,12 +17,48 @@ public class Day22 extends Puzzle {
         solve2();
     }
 
+    private long nextSecret(long secret) {
+        //p1
+        var result = secret * 64;
+        secret = mix(result, secret);
+        secret = prune(secret);
+        //p2
+        result = secret / 32;
+        secret = mix(result, secret);
+        secret = prune(secret);
+        //p3
+        result = secret * 2048;
+        secret = mix(result, secret);
+        secret = prune(secret);
+
+        return secret;
+    }
+
+    private long prune(long secret) {
+        return secret % 16777216;
+    }
+
+    private long mix(long secret, long result) {
+        return secret ^ result;
+    }
+
     private void solve2() {
 
     }
 
     private void solve1() {
+        long sum = 0;
+        for (var line : getInput()) {
+            long secret = Long.parseLong(line);
+            for (var i = 0; i < ITERATIONS; i++) {
+                secret = nextSecret(secret);
+            }
 
+            sum += secret;
+            //log.warn(secret);
+        }
+
+        log.success(sum);
     }
 
     public static void main(String[] ignoredArgs) {
