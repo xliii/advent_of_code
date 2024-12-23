683b02f36111b2dfa20c4e2b958219e1e9a5c2e3
xliii <xliii@mail.com>
2024-12-23 19:37:56 +0200

    Day 24 stub

---
+++ src/main/java/com/xliii/aoc/aoc2024/Day24.java
@@ -0,0 +1,30 @@
+package com.xliii.aoc.aoc2024;
+
+import com.xliii.aoc.Puzzle;
+
+public class Day24 extends Puzzle {
+
+    @Override
+    protected boolean useExample() {
+        return true;
+    }
+
+    @Override
+    protected void run() {
+        solve1();
+        solve2();
+    }
+
+    private void solve2() {
+
+    }
+
+    private void solve1() {
+
+    }
+
+    public static void main(String[] ignoredArgs) {
+        Day24 puzzle = new Day24();
+        puzzle.run();
+    }
+}
