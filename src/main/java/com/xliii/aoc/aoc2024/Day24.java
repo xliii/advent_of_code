02b54e63e35c94f0966363cd283c9d0ac3c21551
xliii <xliii@mail.com>
2024-12-24 18:57:48 +0200

    Day 24.2 semi-automatic solution

--- src/main/java/com/xliii/aoc/aoc2024/Day24.java
+++ src/main/java/com/xliii/aoc/aoc2024/Day24.java
@@ -7,6 +7,38 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Day24 extends Puzzle {
+    
+    
+    //0 simple case
+    //for i = 1; i < 45; i++
+    //xN XOR yN -> partial sum
+    //xN AND yN -> partial carry
+    //partial sum AND carry out (i-1) -> carry's carry
+    //partial sum XOR carry out (i-1) -> sum(i)
+    //partial carry OR carry's carry -> carry out (i)
+    
+    /*
+    x00 XOR y00 -> z00 (0 sum)
+    x00 AND y00 -> gct (carry out 0)
+    
+    x01 XOR y01 -> wnt (1 partial sum)
+    x01 AND y01 -> gcq (1 partial carry)
+    wnt AND gct -> dnc (1 carry's carry)
+    wnt XOR gct -> z01 (1 sum)
+    gcq OR dnc -> qcs (carry out 1)
+
+    x02 XOR y02 -> gwm (2 partial sum)
+    x02 AND y02 -> vhv (2 partial carry)
+    gwm AND qcs -> vhs (2 carry's carry)
+    gwm XOR qcs -> z02 (2 sum)
+    vhv OR vhs -> ktn (carry out 2) 
+
+
+    Solve2 goes through chain of computation,
+    throwing exception in case expected statement is not found
+    Then frame in question can be inspected manually
+    and replacement added to REPLACE_FIX map
+     */
 
     @Override
     protected boolean useExample() {
@@ -25,13 +57,67 @@ public class Day24 extends Puzzle {
         XOR
     }
 
-    private record Statement(String arg1, Op op, String arg2, String target) {}
+    private record Statement(String arg1, Op op, String arg2, String target) {
+        @Override
+        public String toString() {
+            return arg1 + " " + op + " " + arg2 + " -> " + target;
+        }
+    }
 
     private final Map<String, Integer> VARIABLES = new HashMap<>();
     private final List<Statement> STATEMENTS = new ArrayList<>();
+    
+    private final int SIZE = 45;
+    
+    private final String[] PARTIAL_SUMS = new String[SIZE];
+    private final String[] PARTIAL_CARRIES = new String[SIZE];
+    private final String[] CARRY_CARRIES = new String[SIZE];
+    private final String[] SUMS = new String[SIZE];
+    private final String[] CARRIES_OUT = new String[SIZE];
+
+    private Statement find(String arg1, String arg2, Op op) {
+        return STATEMENTS.stream()
+                .filter(s -> s.op.equals(op))
+                .filter(s -> (s.arg1.equals(arg1) && s.arg2.equals(arg2))
+                        || (s.arg1.equals(arg2) && s.arg2.equals(arg1)))
+                .findAny().orElseThrow(() -> new RuntimeException("Couldn't find " + arg1 + " " + op + " " + arg2));
+    }
 
     private void solve2() {
+        init();
 
+        var firstSum = find("x00", "y00", Op.XOR);
+        var firstCarryOut = find("x00", "y00", Op.AND);
+
+        SUMS[0] = firstSum.target;
+        CARRIES_OUT[0] = firstCarryOut.target;
+
+        for (int i = 1; i < SIZE; i++) {
+            String xName = "x" + String.format("%02d", i);
+            String yName = "y" + String.format("%02d", i);
+            var partialSum = find(xName, yName, Op.XOR);
+            var partialCarry = find(xName, yName, Op.AND);
+
+            log.warn(partialSum);
+            log.warn(partialCarry);
+            PARTIAL_CARRIES[i] = partialCarry.target;
+            PARTIAL_SUMS[i] = partialSum.target;
+
+            var carriesCarry = find(PARTIAL_SUMS[i], CARRIES_OUT[i - 1], Op.AND);
+            var sum = find(PARTIAL_SUMS[i], CARRIES_OUT[i - 1], Op.XOR);
+
+            log.warn(carriesCarry);
+            log.warn(sum);
+            CARRY_CARRIES[i] = carriesCarry.target;
+            SUMS[i] = sum.target;
+
+            var carryOut = find(PARTIAL_CARRIES[i], CARRY_CARRIES[i], Op.OR);
+            log.warn(carryOut);
+            CARRIES_OUT[i] = carryOut.target;
+        }
+
+        var answer = REPLACE_FIX.keySet().stream().sorted(Comparator.naturalOrder()).collect(Collectors.joining(","));
+        log.success(answer);
     }
 
     private int calculate(int arg1, Op op, int arg2) {
@@ -59,6 +145,7 @@ public class Day24 extends Puzzle {
 
                 var arg1 = VARIABLES.get(statement.arg1);
                 var arg2 = VARIABLES.get(statement.arg2);
+                //log.error(statement);
                 VARIABLES.put(statement.target, calculate(arg1, statement.op, arg2));
                 it.remove();
             }
@@ -78,6 +165,21 @@ public class Day24 extends Puzzle {
         log.success(Long.valueOf(binary, 2));
     }
 
+    private final Map<String, String> REPLACE_FIX = Map.of(
+            "z12", "qdg",
+            "qdg", "z12",
+            "z19", "vvf",
+            "vvf", "z19",
+            "fgn", "dck",
+            "dck", "fgn",
+            "z37", "nvh",
+            "nvh", "z37"
+    );
+
+    private String fix(String input) {
+        return REPLACE_FIX.getOrDefault(input, input);
+    }
+
     private void init() {
         VARIABLES.clear();
         STATEMENTS.clear();
@@ -94,14 +196,14 @@ public class Day24 extends Puzzle {
             VARIABLES.put(parts[0], Integer.parseInt(parts[1]));
         }
 
-        log.error(VARIABLES);
+        //log.error(VARIABLES);
 
         for (var statement : statements) {
             String[] parts = statement.split(" ");
-            STATEMENTS.add(new Statement(parts[0], Op.valueOf(parts[1]), parts[2], parts[4]));
+            STATEMENTS.add(new Statement(parts[0], Op.valueOf(parts[1]), parts[2], fix(parts[4])));
         }
 
-        log.error(STATEMENTS);
+        //log.error(STATEMENTS);
     }
 
     public static void main(String[] ignoredArgs) {
